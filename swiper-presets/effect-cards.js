5d65954dcd70d925635c37529b99ba142cd356b8
atmosfera2000 <atmosfera2000@mail.ru>
2024-11-10 18:26:23 +0300

    effect-cards

--- swiper-presets/effect-cards.js
+++ swiper-presets/effect-cards.js
@@ -6,10 +6,6 @@ import 'swiper/scss/effect-cards';
 import './style.scss';
 
 const SWIPER_CSS = '.swiper';
-const SWIPER_CSS_NEXT = '.swiper-button-next';
-const SWIPER_CSS_PREV = '.swiper-button-prev';
-const SWIPER_CSS_PAGINATION = '.swiper-pagination';
-
 
 const swiper = new Swiper(SWIPER_CSS, {
     modules: [EffectCards],
